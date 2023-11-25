014d2bf330c2cc5f63a0a54a9a41fa8015febd2b
EOussama <oussama_gtacj@live.fr>
2023-11-25 14:14:36 +0100

    Setup

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker compose -f src/docker-compose.yml up
--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Start the Oracle Database
+# Print listener status for debugging
 $ORACLE_HOME/bin/lsnrctl start
-$ORACLE_HOME/bin/sqlplus / as sysdba <<EOF
-  ALTER PLUGGABLE DATABASE ALL OPEN;
-  exit;
-EOF
+
+# Connect with SQL*Plus
+# $ORACLE_HOME/bin/sqlplus / as sysdba <<EOF
+# $ORACLE_HOME/bin/sqlplus sys/very_STRONG_p055w0rd@pdb3 as sysdba <<EOF
+#   SELECT * FROM all_tables WHERE ROWNUM <= 2;
+#   exit;
+# EOF
 
 # Keep the container running
-tail -f /dev/null
+# tail -f /dev/null
+$ORACLE_HOME/bin/sqlplus sys/very_STRONG_p055w0rd@pdb3 as sysdba
